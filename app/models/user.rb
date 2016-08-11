c5256dd1ff601504fc3a2b123f9f0deb18957499
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-10 18:51:55 -0700

    Add colon before :users in users migration file, add bcrypt logic and user authentication to User model, add action and method to form in users/new

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,19 @@ class User < ActiveRecord::Base
   validates :username, uniqueness: true, presence: true
   validates :email, presence: true
 
-  include 'BCrypt'
+  include BCrypt
 
-  has_secure_password
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def authenticate?(input_password)
+    self.password == input_password
+  end
 end
 
