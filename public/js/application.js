b735a2263b298a4356a0313714e19f23bd60de1f
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-13 13:49:08 -0700

    Start javascript notes and functionality for canvas

--- public/js/application.js
+++ public/js/application.js
@@ -5,3 +5,10 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+var canvas = document.getElementById('canvas'); //this gets the canvas element. I am setting it to a variable so that I can call canvas functions on it!
+var context = canvas.getContext('2d'); //this sets the area coordinates of the canvas, aka the drawing space based on the canvas size and placement
+// The context is 2d because this specifies the use of the 2d API rather than the 3D api. The 3d api is beyond my understanding at this point
+context.lineWidth = 5; //this sets the width of the drawing tool. Add additional sizes in additional releases of app.
+var down = false;
+
