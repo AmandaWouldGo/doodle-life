eb5ba65390978cf840e025170f9487062b9cd6c4
AmandaWouldGo <amandawouldgo@gmail.com>
2016-08-13 14:08:59 -0700

    Remove random left angle bracket from layout.erb and add x y coordinates, drawing function and eventlistener for mousemove to js file

--- public/js/application.js
+++ public/js/application.js
@@ -10,5 +10,16 @@ var canvas = document.getElementById('canvas'); //this gets the canvas element. 
 var context = canvas.getContext('2d'); //this sets the area coordinates of the canvas, aka the drawing space based on the canvas size and placement
 // The context is 2d because this specifies the use of the 2d API rather than the 3D api. The 3d api is beyond my understanding at this point
 context.lineWidth = 5; //this sets the width of the drawing tool. Add additional sizes in additional releases of app.
-var down = false;
+var down = false; //this will be connected to events when the mouse is pressed!
+
+//next... add eventListeners to canvas itself! This is different than adding event listers to the document!
+
+canvas.addEventListener('mousemove', draw);
+
+function draw(event) {
+  xPosition = event.clientX - canvas.offsetLeft; //sets the x position of the mouse as it moves along inside the canvas
+  yPosition = event.clientY - canvas.offsetTop; //sets the y position of the mouse as it moves along inside the canvas
+};
+
+
 
